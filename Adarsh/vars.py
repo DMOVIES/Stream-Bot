19268c6630e1bfd1b3ec83f587003e69ac1b5529
DMOVIES <150237980+DMOVIES@users.noreply.github.com>
2023-12-11 22:41:53 +0530

    var

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
-    name = str(getenv('name', 'Nobita-Stream-Bot'))
+    API_ID = int(getenv('API_ID', '24849668'))
+    API_HASH = str(getenv('API_HASH', '5034fe2d5200d2a80177db82965cc45e'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6861408977:AAFKZFWOAz0AsSPg36LXO38gp2Wd9Cr2DRc'))
+    name = str(getenv('name', 'Doffice-stream-bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", ""))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5412093415").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'NobiDeveloperr'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'd_movie_office'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -34,6 +34,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://stream:stream@cluster0.pd23blr.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
